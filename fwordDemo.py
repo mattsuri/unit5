491285df83d81f151dfc538bf101a6b2a0c025fc
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-23 08:21:03 -0400

    Updated from Brython Server: 4/23/2018 8:21:03 AM

--- fwordDemo.py
+++ fwordDemo.py
@@ -1,1 +1,10 @@
+#Matthew Suriawinata
+#4/23/18
+#fwordDemo.py - print out words with f
 
+
+words = input("Type in some wordsL ").split(" ")
+
+for item in words:
+    if "f" in item or "F" in item:
+        print(item)
