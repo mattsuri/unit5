2fbd152c99de093085e9a22a758fdf551480a709
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-25 08:18:45 -0400

    Updated from Brython Server: 4/25/2018 8:18:49 AM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,19 @@
+#Matthew Suriawinata
+#4/25/18
+#warmup13.py 
+
+
+from random import randint
+
+numbers = []
+for i in range 20:
+    numbers.append(randint(1,100)) 
+    
+print(numbers)
+    
+print(sum(numbers))
+print(min(numbers))
+print(max(numbers))
+
+
 
