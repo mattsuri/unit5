10e208b776c2dab288b560f3166c261c6735c1fe
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-25 08:34:48 -0400

    Updated from Brython Server: 4/25/2018 8:34:52 AM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -5,11 +5,19 @@
 from random import randint
 from ggame import *
 
+#ffffd9	(255,255,217)
+#176a90	(23,106,144)
+#00122e	(0,18,46)
+#969281	(150,146,129)
+#bf5449	(191,84,73)
+
+
+
 
 
 def mouseClick(event):
     
-    colors = [0xFF0000, 0xFFF000, 0xFFFF00, 0xFFFFF0, 0xFFFFFF]
+    colors = [0x176a90, 0xffffd9, 0x00122e, 0x969281, 0xbf5449]
     
     color = colors[randint(0,4)]
     
