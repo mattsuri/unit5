610c97f93e20fb1438c0d8607a1011216b96fb2f
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-25 08:13:04 -0400

    Updated from Brython Server: 4/25/2018 8:13:08 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,11 @@
+#Matthew Suriawinata
+#4/25/18
+#dictionaryDemo.py - more list practice
+
+words = ["computer", "mortify", "dog", "firetruck", "yes", "python", "cat"]
+
+words.sort()
+
+num = int(input("What number owrd to you want?"))
+print(words[num-1])
 
