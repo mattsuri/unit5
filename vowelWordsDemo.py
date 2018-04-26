fb20c8dbdb91be5026a9cefe246f696ff7eec131
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-26 08:30:46 -0400

    Updated from Brython Server: 4/26/2018 8:30:51 AM

--- vowelWordsDemo.py
+++ vowelWordsDemo.py
@@ -1,1 +1,9 @@
+#Matthew Suriawinata
+#4/26/18
+#vowelWordDemo.py - treat strings as lists 
 
+words = input("Type in some words: ").split(" ")
+
+for w in words:
+    if w[0] in "AEIOUaeiou":
+        print(w)
