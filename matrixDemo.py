d44da9969879bd499a4956ea2ff1692f84d3af80
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-27 08:30:06 -0400

    Updated from Brython Server: 4/27/2018 8:30:09 AM

--- matrixDemo.py
+++ matrixDemo.py
@@ -2,4 +2,13 @@
 #4/27/18
 #matrixDemo.py - lists inside of lists 
 
+
+def printBoard(board):
+    for r in range(0,3):
+        for c in range(0,3):
+            print(board[r][c],end=" ")
+        print()
+
 board = [["a","b","c"],["d","e","f"],["g","h","i"]]
+
+printBoard(board)
