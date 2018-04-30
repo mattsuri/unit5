3816733241ab13ac841fb1513a41d0977a7242d1
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-30 08:26:46 -0400

    Updated from Brython Server: 4/30/2018 8:26:52 AM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,14 @@
+#Matthew Suriawinata
+#4/30/18
+#wamrup14.py
+
+name = input("Type in your first and last name: ").split(" ")
+
+initials = []
+
+for w in name:
+    initials.append(w[0])
+        
+
+print("Your initlas are", initials[0], ".", initials[1], ".")
 
