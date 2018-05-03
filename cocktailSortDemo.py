5ee4b922651d7eb826a1cea20d5487574849cc0a
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-05-03 08:17:06 -0400

    Updated from Brython Server: 5/3/2018 8:17:28 AM

--- cocktailSortDemo.py
+++ cocktailSortDemo.py
@@ -8,23 +8,22 @@ from time import time
 N = 10 #how many numbers will be sorted
 
 def mySort(A):
-    swapped = True:
-        while swapped:
-            swapped = False
-            for i in range(0,len(A)-1)
-                if A[i] > A[i+1]:
-                    A[i], A[i+1] = A[i+1], A[i] #swap in python
-                    swapped = True
-            if not swapped:
-                break
-            swapped - False
-            for i in range(len(A)-2,-1,-1)
-                if A[i] > A[i+1]:
-                    A[i], A[i+1] = A[i+1], A[i] #swap in python
-                    swapped = True
-            if not swapped:
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(0,len(A)-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i] #swap in python
+                swapped = True
+        if not swapped:
+            break
+        swapped - False
+        for i in range(len(A)-2,-1,-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i] #swap in python
+                swapped = True
+        
             
-    
     
     return A
     
@@ -39,7 +38,7 @@ if __name__ == '__main__':
     
     #time how long your sort takes
     t1 = time()
-    numbers = mySort(numbers)
+    numbers = numbers.sort()
     t2 = time()
     
     #print whether the sort worked or not
