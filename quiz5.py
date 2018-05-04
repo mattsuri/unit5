d0bce56f4e0e50a40e6e2efbbcc6e59570d2fb27
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-05-04 09:00:17 -0400

    Updated from Brython Server: 5/4/2018 9:00:54 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,47 @@
+#Matthew Suriawinata
+#5/4/18
+#quiz5.py - 
 
+
+def penultimate(list):
+    pen = len(list) -2
+    return(list[pen])
+    
+    
+print(penultimate([3,4,5,6,7]))
+
+def plusEquals(numbers,integer):
+    i = 0
+    added = []
+    while i < len(numbers):
+        num = numbers[i] + integer
+        added.append(num)
+        i += 1
+
+        return(added)
+        
+print(plusEquals([1,2,3,4],10))
+
+def smallest(numbers):
+    small = 0
+    run = 1
+    for i in numbers:
+        if i < small or run == 1:
+            small = i
+            run +=1
+        return(small)
+    
+print(smallest([1,2,3,4]))
+
+
+def decimalRange(low, high, step):
+    list = []
+    current = low
+    list.append(low)
+    while current < high:
+        current = current + step
+        list.append(current)
+        
+    return list
+
+print(decimalRange(4,10,0.5))
